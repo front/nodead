07a7d344400785f8c2ead028fbbfcc428c7eb500
Joakim Stai <joakimstai@gmail.com>
2012-11-27 21:15:15 +0100

    Separated socket handler and ads logic into files
    Also improved the ads retrieval logic, to fix bug
    with being locked into one category after first
    like/dislike (yes, even if you dislike).

--- app.js
+++ app.js
@@ -8,9 +8,8 @@ var app = express();
 
 var utils = require('./utils'),
     db = require('./db'),
-    routes = require('./routes');
-
-var testAds = require('./testdata.json');
+    routes = require('./routes'),
+    sockets = require('./sockets');
 
 // Sessions.
 var sessionSecret = 'the cake is a lie',
@@ -46,97 +45,18 @@ var server = http.createServer(app),
 // Start up the engine.
 server.listen(app.get('port'));
 
-// Returns a random ad from one of the user's liked categories.
-var getRandomAd = function (sid, callback) {
-  async.waterfall([
-    function getLikedCategories (callback) {
-      var args = ['user:' + sid, '+inf', '-inf', 'limit', 0, 2];
-      db.zrevrangebyscore(args, callback);
-    },
-    function getAdsFromCategories (categories, callback) {
-      var args = [];
-      categories.forEach(function (category) {
-        args.push('index:' + category);
-      });
-      db.sunion(args, callback);
-    },
-    function getRandomAd (ads, callback) {
-      var id = ads[Math.floor(Math.random() * ads.length)];
-      callback(null, id);
-    }
-  ],
-  function done (err, id) {
-    if (err) return callback(err);
-    callback(err, testAds[id]);
-  });
-};
-
 // Listen for socket connections.
 io.set('log level', 1);
 io.sockets.on('connection', function (socket) {
-  // Store the client's session ID on connection.
+  // Store the client's session ID.
   cookieParser(socket.handshake, {}, function (err) {
-    var sid = socket.handshake.signedCookies['connect.sid'];
-    socket.set('sid', sid);
-    console.log(sid + ' connected');
+    socket.set('sid', socket.handshake.signedCookies['connect.sid']);
   });
 
-  // Send the first ad to the client.
-  socket.emit('ad', testAds[1]);
-
-  // Like an ad.
-  socket.on('like', function (data) {
-    var ad = testAds[data.id];
-
-    socket.get('sid', function (err, sid) {
-      if (err) return;
-
-      console.log(sid + ' seems to really like item ' + data.id + ' so much that he stayed on it for ' + data.timeOnAd/1000 + ' seconds');
-
-      db.zincrby('user:' + sid, 1, ad.category, function (err, score) {
-        console.log(sid + ' liking ' + ad.category + ': ' + score);
-      });
-
-      getRandomAd(sid, function (err, ad) {
-        if (err) return;
-        socket.emit('ad', ad);
-      });
-    })
-  });
-
-  // Dislike an ad.
-  socket.on('dislike', function (data) {
-    var ad = testAds[data.id];
-
-    socket.get('sid', function (err, sid) {
-      if (err) return;
-
-      console.log(sid + ' seems to really hate item' + data.id + ' so much that he stayed on it for ' + data.timeOnAd/1000 + ' seconds');
-
-      db.zincrby('user:' + sid, -1, ad.category, function (err, score) {
-        console.log(sid + ' liking ' + ad.category + ': ' + score);
-      });
-
-      getRandomAd(sid, function (err, ad) {
-        if (err) return;
-        socket.emit('ad', ad);
-      });
-    })
-  });
-
-  // Get a random ad from the user's liked categories.
-  socket.on('next', function (data) {
-    socket.get('sid', function (err, sid) {
-      getRandomAd(sid, function (err, ad) {
-        if (err) return;
-        socket.emit('ad', ad);
-      });
-    });
-  });
-
-});
 
 //redis.debug_mode = true;
 db.on("error", function (err) {
   console.log("Error " + err);
+  // Delegate to connection handler.
+  sockets.connection(socket);
 });
