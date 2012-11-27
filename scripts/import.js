9a824d3bab4635162e3c80097eb12c53ccb75804
Joakim Stai <joakimstai@gmail.com>
2012-11-27 21:16:13 +0100

    Fixed import to work with new index structure

--- scripts/import.js
+++ scripts/import.js
@@ -1,15 +1,15 @@
 var utils = require('./utils'),
     db = require('./db');
 
-var testAds = require('../testdata.json');
+var ads = require('../testdata.json');
 
 utils.async.series({
   // Insert test ads into the 'ad' hashtable.
   import: function (callback) {
     var multi = db.multi();
 
-    for (var id in testAds) {
-      var ad = testAds[id];
+    for (var id in ads) {
+      var ad = ads[id];
 
       var args = ['ad:' + id];
 
@@ -29,16 +29,20 @@ utils.async.series({
   index: function (callback) {
     var multi = db.multi();
 
-    for (var id in testAds) {
-      var ad = testAds[id];
-      multi.sadd('index:' + ad.category, id);
+    for (var id in ads) {
+      var ad = ads[id];
+      multi.sadd('index:all', id);
+      multi.sadd('index:category:' + ad.category, id);
+      multi.sadd('index:gender:' + ad.gender, id);
     }
 
     multi.exec(callback);
   }
 }, function done (err, results) {
   if (err) console.log('Error: ' + err);
-  console.log('Imported: ' + results.import.length);
-  console.log('Indexed: ' + results.index.length);
+
+  console.log('Ads: ' + results.import.length);
+  console.log('Indexes: ' + results.index.length);
+
   process.exit(err ? 1 : 0);
 });
