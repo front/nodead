4838ef71dc488f54b011f19bbae522e9d554eefd
Henrik Akselsen <henrik.akselsen@gmail.com>
2012-11-28 16:05:03 +0100

    Change pointer to hand for facebook link

--- static/js/aftenposten_custom.js
+++ static/js/aftenposten_custom.js
@@ -2,6 +2,7 @@ $(function () {
 
 
   var socket = io.connect();
+	$('div#facebook_emitter').css("cursor", "pointer");
 
 	$('div#facebook_emitter').on('click', function(){
     socket.emit('facebook-like', {"category": "travel"});
