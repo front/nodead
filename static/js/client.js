5f9e612046e5f9601d596bd2cfdf2d7d5c09fb06
Henrik Akselsen <henrik.akselsen@gmail.com>
2012-11-28 11:54:09 +0100

    Some refactoring of client side js

--- static/js/client.js
+++ static/js/client.js
@@ -1,19 +1,15 @@
 $(function () {
   var socket = io.connect();
+  
+  // Holds data for current shown ad
   var current;
-  socket.on('ad', function (data) {
-    console.log(data);
 
-   var newListEl =  '<li class="ad"><h2>' + data.ad.title + '</h2><img src="' + data.ad.img + '"><span class="slide-text">← Slide me</span></li>';
-    $("ul#ads").append(newListEl);
-    $('ul#ads li:first').remove();
+  socket.on('ad', function (data) {
+    var el = constructNewAdElement(data);
+    addNewListElement(el);
 
     current = data.ad;
     current.timeStart = new Date().getTime();
-    console.log('current:');
-    console.log(current);
-
-
   });
 
   $('#like').on('click', function () {
@@ -28,7 +24,7 @@ $(function () {
     socket.emit('dislike', {"id": current.id, "timeOnAd": timeOnAd});
   });
 
- document.querySelector('#man-toggle')
+  document.querySelector('#man-toggle')
   .addEventListener('toggle', function(e){
     if (e.detail.isActive){
       socket.emit('gender', 'men');
@@ -36,4 +32,20 @@ $(function () {
       socket.emit('gender', 'both');
     }
   });
+
+  function constructNewAdElement(data){
+    var el = $('<li>',{ 
+      class:"ad"});
+    el.append( '<h2>' + data.ad.title + '</h2>');
+el.append('<img src="' + data.ad.img + '">');
+el.append('<span class="slide-text">← Slide me</span>');
+ //   return '<li class="ad"><h2>' + data.ad.title + '</h2><img src="' + data.ad.img + '"><span class="slide-text">← Slide me</span></li>';
+ return el;
+  }
+
+  function addNewListElement(el){
+    $("ul#ads").append(el);
+    $('ul#ads li:first').remove();
+  }
+
 });
