3461daaba19e4bd90347c5613835fb4448c5f600
Joakim Stai <joakimstai@gmail.com>
2012-11-29 13:14:30 +0100

    Make article identify itself as the slave that it is
    *brings out whip*

--- static/js/aftenposten_custom.js
+++ static/js/aftenposten_custom.js
@@ -1,7 +1,15 @@
 $(function () {
-
-
   var socket = io.connect();
+
+  // Identify this connection with the server.
+  socket.on('connect', function () {
+    socket.emit('identify', {
+      role: 'slave',
+      ads: 3
+    });
+  });
+
+
 	$('div#facebook_emitter').css("cursor", "pointer");
 
 	$('div#facebook_emitter').on('click', function(){
