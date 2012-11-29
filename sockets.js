08ea3cf25fd5273d4c8dc0a401cebc690de4fabb
Joakim Stai <joakimstai@gmail.com>
2012-11-29 10:06:08 +0100

    Support multiple connections per socket client
    And work with refactored ads.js

--- sockets.js
+++ sockets.js
@@ -27,63 +27,76 @@ var connection = function (socket) {
   // Pass settings on to the client.
   socket.emit('settings', ads.settings);
 
-        // Return new ads for this user.
-        ads.getByProfile(3, socket, function (err, data) {
-          if (err) return console.log(err);
-          socket.emit('ad', data);
-        });
-      });
+  // Send the first ad to the client.
+  socket.emitAds(socket);
+
+  // Join the room for this user session.
+  socket.join(socket.sid);
+
+  // User likes an ad.
+  socket.on('like', function (data) {
+    ads.updateScore(socket, data.id, 1, function (err, category) {
+      if (err) return console.log(err);
+      console.log(socket.sid + ' on ' + category.name + ': ' + category.score + ' (like after ' + Math.floor(data.timeOnAd / 1000) + ' seconds)');
+      socket.emitAds();
     });
   });
 
-  // User dislikes an ad.
+  // User doesn't like an ad.
   socket.on('dislike', function (data) {
-    // First load the ad and user's session ID.
-    utils.async.parallel({
-      ad: ads.load.bind(ads, data.id),
-      sid: socket.get.bind(socket, 'sid')
-    },
-    function (err, results) {
+    ads.updateScore(socket, data.id, -1, function (err, category) {
       if (err) return console.log(err);
-
-      var ad = results.ad, sid = results.sid;
-
-      // Decrement user's love for the category. Or increment hate, if you like.
-      db.zincrby('user:' + sid + ':categories', -1, ad.category, function (err, score) {
-        if (err) return console.log(err);
-
-        console.log(sid + ' on ' + ad.category + ': ' + score + ' (dislike after ' + Math.floor(data.timeOnAd/1000) + ' seconds)');
-
-        // Return new ads for this user.
-        ads.getByProfile(3, socket, function (err, data) {
-          if (err) return console.log(err);
-          socket.emit('ad', data);
-        });
-      });
-    })
-  });
-
-  // User wants more ads.
-  socket.on('next', function (data) {
-    ads.getByProfile(3, socket, function (err, ad) {
-      if (err) return console.log(err);
-      socket.emit('ad', ad);
+      console.log(socket.sid + ' on ' + category.name + ': ' + category.score + ' (dislike after ' + Math.floor(data.timeOnAd / 1000) + ' seconds)');
+      socket.emitAds();
     });
   });
 
-  socket.on('gender', function (name) {
-    socket.set('gender', name);
-  });
-
-  // If a client clicks a facebook like, increase he's like of that category by 10
+  // If client clicks a facebook like, increase he's like of that category by 10
   socket.on('facebook-like', function (data){
     console.log(data);
-    socket.get('sid', function (err, sid) {
+    if (err) return console.log(err);
+    db.zincrby('user:' + socket.sid + ':categories', 10, data.category, function (err, score) {
       if (err) return console.log(err);
-      db.zincrby('user:' + sid + ':categories', 10, data.category, function (err, score) {
-        if (err) return console.log(err);
-        console.log(sid + ' on ' + data.category + ': ' + score + ' (like after ' + Math.floor(data.timeOnAd/1000) + ' seconds)');
-      });
+      console.log(socket.sid + ' on ' + data.category + ': ' + score + ' (like after ' + Math.floor(data.timeOnAd/1000) + ' seconds)');
     });
+  });
+
+  // User can't get enough of those ads.
+  socket.on('next', function (data) {
+    socket.emitAds();
+  });
+
+  // User has changed gender. (Say what now??)
+  socket.on('gender', function (gender) {
+    socket.gender = gender;
+  });
+
+  socket.on('disconnect', function () {
+    console.log(socket.sid + ' disconnected');
+  });
+}
+
+// Returns the socket client's session ID from Express.
+socketio.Socket.prototype.getSid = function (cookieParser, callback) {
+  var socket = this;
+  cookieParser(socket.handshake, {}, function (err) {
+    callback(err, socket.handshake.signedCookies['connect.sid']);
+  });
+}
+
+// Emits new ads for this socket connection.
+socketio.Socket.prototype.emitAds = function (recipients) {
+  var socket = this;
+
+  if (utils.isUndefined(recipients)) {
+    recipients = socket.in(socket.sid);
+  }
+
+  // TODO Not working, recipients should be array of clients connected to room.
+  var count = utils.isArray(recipients) ? recipients.length : 1;
+
+  ads.getByProfile(socket, count, function (err, data) {
+    if (err) return console.log(err);
+    recipients.emit('ads', data);
   });
 };
